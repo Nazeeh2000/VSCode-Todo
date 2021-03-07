8209ee2ab6e27c0402e77cc11bb2471be77ce806
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-03-08 00:44:59 +0530

    Integrate functionality to add text from textEditor to todo

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,14 @@ import { SidebarProvider } from './SidebarProvider';
 export function activate(context: vscode.ExtensionContext) {
   // console.log('Congratulations, your extension "vstodo" is now active!');
 
+  const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+  item.text = "$(add) Add todo";
+  item.command = 'vstodo.addTodo';
+  item.show();
+
   const sidebarProvider = new SidebarProvider(context.extensionUri);
   context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      "vstodo-sidebar",
-      sidebarProvider
-    )
+    vscode.window.registerWebviewViewProvider('vstodo-sidebar', sidebarProvider)
   );
 
   context.subscriptions.push(
@@ -20,11 +22,34 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(
+    vscode.commands.registerCommand('vstodo.addTodo', () => {
+      const { activeTextEditor } = vscode.window;
+
+      if (!activeTextEditor) {
+        vscode.window.showInformationMessage('No active text editor');
+        return;
+      }
+
+      const text = activeTextEditor.document.getText(
+        activeTextEditor.selection
+      );
+
+      vscode.window.showInformationMessage('Text: ' + text);
+      sidebarProvider._view?.webview.postMessage({
+        type: 'new-todo',
+        value: text,
+      });
+    })
+  );
+
+  context.subscriptions.push(
     vscode.commands.registerCommand('vstodo.refresh', async () => {
       // HelloWorldPanel.kill();
       // HelloWorldPanel.createOrShow(context.extensionUri);
-      await vscode.commands.executeCommand("workbench.action.closeSidebar");
-      await vscode.commands.executeCommand("workbench.view.extension.vstodo-sidebar-view");
+      await vscode.commands.executeCommand('workbench.action.closeSidebar');
+      await vscode.commands.executeCommand(
+        'workbench.view.extension.vstodo-sidebar-view'
+      );
       // setTimeout(() => {
       //   vscode.commands.executeCommand(
       //     'workbench.action.webview.openDeveloperTools'
